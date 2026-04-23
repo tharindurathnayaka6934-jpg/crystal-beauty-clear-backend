e87cd7bd6fd653c757e2affea98ddf86ae997c64
tharindurathnayaka6934-jpg <tharindurathnayaka6934@gmail.com>
2026-04-23 11:42:14 +0530

    dotenv implimentation 👌

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ import jwt from 'jsonwebtoken';
 import productRouter from './routes/productRouter.js';
 import verifyJWT from './middlewares/auth.js';
 import orderRouter from './routes/orderRouter.js';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
 
-mongoose.connect("mongodb://adminTharindu:123abc@ac-17ph1vk-shard-00-00.40i1k8g.mongodb.net:27017,ac-17ph1vk-shard-00-01.40i1k8g.mongodb.net:27017,ac-17ph1vk-shard-00-02.40i1k8g.mongodb.net:27017/?ssl=true&replicaSet=atlas-7a71gq-shard-0&authSource=admin&appName=Cluster0").then(
+mongoose.connect(process.env.MONGO_URL).then(
     () =>{
         console.log("Connect to the database");
     }
